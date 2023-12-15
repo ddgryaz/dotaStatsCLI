62053eeaa91a67af7760ffcc69849397dc20d21a
Dmitriy Dzyuman <dzuman1996@mail.ru>
2023-12-15 13:05:25 +0500

    CORE/delete row parser for json document

--- src/parser.ts
+++
@@ -1,9 +0,0 @@
-import { parserDotaBuff } from "./core/parserDotaBuff";
-import { writeFile } from "fs/promises";
-
-(async function start() {
-  await writeFile(
-    "result.json",
-    JSON.stringify(await parserDotaBuff(64015685, 100)),
-  );
-})();
