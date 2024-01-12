0d4da9e2cff4de2bcfa3adfea129772a7f68f0a6
Dmitriy Dzyuman <dzuman1996@mail.ru>
2024-01-12 13:51:46 +0500

    1.3.0/OPENDOTA/fix MIN_DURATION and comment

--- src/core/openDota/calcRecordsFromOpenDota.ts
+++ src/core/openDota/calcRecordsFromOpenDota.ts
@@ -13,8 +13,9 @@ const getMatchUrl = (matchId: number): string => {
 /*
  * seconds * minutes = duration in seconds.
  * A minimum value has been set to prevent such games from being included in records - https://www.dotabuff.com/matches/1492991064
+ * 450 sec (7.5 minutes) - the time when the aghanim's shard will be available for purchase in turbo mode.
  */
-const MIN_DURATION: number = 60 * 6;
+const MIN_DURATION: number = 450;
 
 export async function calcRecordsFromOpenDota(
   arrayWithGames: IAllMatches[],
